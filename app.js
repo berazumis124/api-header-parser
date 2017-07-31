8e72f6c0338d8e28d6dbf6cfe0b360a26b454b44
berazumis124 <berazumis@gmail.com>
2017-07-31 11:53:38 +0300

    Added files to track

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var accepts = require('accepts');
+var useragent = require('express-useragent');
+var PORT = process.env.PORT || 4998;
+var resJson = {};
+
+app.get('*', function(req, res){
+    //console.log(req.ip);
+    var source = req.headers['user-agent'], ua = useragent.parse(source);
+    //console.log(JSON.stringify(ua));
+    //console.log(ua.os);
+    var accept = accepts(req);
+    //console.log(accept.language()[0]);
+    //console.log(req.connection.remoteAddress);
+    resJson.ipaddress = req.connection.remoteAddress;
+    resJson.os = ua.os;
+    resJson.language = accept.language()[0];
+    res.status(200).json(resJson);
+    //console.log(JSON.stringify(req.headers));
+});
+
+
+app.listen(PORT);
